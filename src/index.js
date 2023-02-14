a6f55e0d57f29ee7328c2a41d023685289dd3cd7
Maxim <mihalenko.maxim21@gmail.com>
2023-02-15 00:42:18 +0300

    update cors

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ const models = require("./models");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://spontaneous-ganache-f53816.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/", router);
 
