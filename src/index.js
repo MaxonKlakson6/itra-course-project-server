2b6251f37b49f37687cfd424854b78a7fccae592
Maxim <mihalenko.maxim21@gmail.com>
2023-02-14 23:01:33 +0300

    change cors configuration

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ const models = require("./models");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/", router);
 
